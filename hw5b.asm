6220391155c0c3468bf5217c43f2caee17b9c48d
Thao Phan <phant1900@hotmail.com>
2017-11-06 10:09:10 -0500

    commiting hw4d.asm, hw5a.asm, hw5b.asm, hw5c.asm

---
+++ hw5b.asm
@@ -0,0 +1,67 @@
+;;; ; hw5b.asm
+;;; ; Thao Phan
+;;; ;
+;;; ; the program prompts user for a string, and prints it back several
+;;; ; times. The number of times string is printed is same as
+;;; ; number of characters in the string.
+;;; ;
+;;; ; Modification: It removes the last character of the previous
+;;; ; printed string as it goes through the loop:
+;;; ;
+;;; ; to assemble and run:
+;;; ;
+;;; ;     nasm -f elf -F  stabs program.asm
+;;; ;     ld -melf_i386 -o program program.o
+;;; ;     ./program
+;;; ; -------------------------------------------------------------------
+
+	;; %include files here...
+
+;;;  ------------------------------------------------------------
+;;;  data areas
+;;;  ------------------------------------------------------------
+	section .data
+	prompt          db      "> "
+	address		dd	" "
+
+;;;  ------------------------------------------------------------
+;;;  code area
+;;;  ------------------------------------------------------------
+
+		section .text
+        	global  _start
+		extern	_getString
+		extern	_printString
+		extern	_println
+		extern	_printRegs
+	
+_start:
+	;; prompt the user
+		mov	ecx, prompt
+		mov	edx, 2
+		call	_printString
+	
+	;;;  get a string from the user
+		call    _getString
+	
+
+ 		mov	dword[address], ecx	; stores string address in memory
+		mov	ecx, edx		; set counter in ecx
+
+loopthis:
+		mov	esi, ecx		; save counter value in esi
+		mov	ecx, dword[address]	; move address of string into ecx
+		call	_printString		; print the string!!!
+		call	_println	     
+		mov	ecx, esi		; move current value of counter back into ecx
+		dec	edx			; decrement edx reg so that it removes the last char in string!!!
+		loop	loopthis
+		
+
+
+;;;  exit()
+
+	        mov     eax,1
+	        mov     ebx,0
+	        int     0x80	; final system call
+
---
+++ hw5c.asm
@@ -0,0 +1,66 @@
+;;; ; hw5c.asm
+;;; ; Thao Phan
+;;; ;
+;;; ; the program prompts user for a string, and prints it back several
+;;; ; times. The number of times string is printed is same as
+;;; ; number of characters in the string.
+;;; ;
+;;; ; Modification: It grows the string from 1 char to it's limit.
+;;; ;
+;;; ; to assemble and run:
+;;; ;
+;;; ;     nasm -f elf -F  stabs program.asm
+;;; ;     ld -melf_i386 -o program program.o
+;;; ;     ./program
+;;; ; -------------------------------------------------------------------
+
+	;; %include files here...
+
+;;;  ------------------------------------------------------------
+;;;  data areas
+;;;  ------------------------------------------------------------
+	section .data
+	prompt          db      "> "
+	address		dd	" "
+
+;;;  ------------------------------------------------------------
+;;;  code area
+;;;  ------------------------------------------------------------
+
+		section .text
+        	global  _start
+		extern	_getString
+		extern	_printString
+		extern	_println
+		extern	_printRegs
+	
+_start:
+	;; prompt the user
+		mov	ecx, prompt
+		mov	edx, 2
+		call	_printString
+	
+	;;;  get a string from the user
+		call    _getString
+	
+
+ 		mov	dword[address], ecx ; stores string address in memory
+ 		
+		mov	ecx, edx		; set counter in ecx
+		mov 	edx, 1 			; move 1 as the number of chars into edx!
+loopthis:
+		mov	esi, ecx		; save counter value in esi
+		mov	ecx, dword[address]	; move address of string into ecx
+		call	_printString		; print the string!!!
+		call	_println	     
+		mov	ecx, esi		; move current value of counter back into ecx
+		inc	edx			; increment edx so that it grows the string!!!
+		loop	loopthis
+
+	
+
+;;;  exit()
+
+	        mov     eax,1
+	        mov     ebx,0
+	        int     0x80	; final system call
